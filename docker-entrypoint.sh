81991570bd052973f5ffbb1b080226e30d2e911a
Fernão Lopes <fernao@riseup.net>
2018-04-20 15:37:29 -0300

    [UPDATE] Ajustes docker-entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,9 +3,10 @@ set -e
 if ! [ -d "/var/www/portal-vale-cultura" ] || ! [ -d "/var/www/portal-vale-cultura/projeto" ]; then
     git clone $GIT_REPOSITORY /tmp/portal-vale-cultura
 
-    cp /tmp/portal-vale-cultura -r /var/wwwB
+    cp /tmp/portal-vale-cultura -r /var/www
 
     cp /var/www/portal-vale-cultura/projeto/application/configs/application.ini-example /var/www/portal-vale-cultura/projeto/application/configs/application.ini
+    cp /var/www/portal-vale-cultura/projeto/application/configs/db.ini-example /var/www/portal-vale-cultura/projeto/application/configs/db.ini
     chown www-data:www-data -R /var/www/portal-vale-cultura
 fi
 
