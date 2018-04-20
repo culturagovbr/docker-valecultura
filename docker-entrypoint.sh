f9df118cb7d39963875ef5bdc1083a9c11f248c2
Fernão Lopes <fernao@riseup.net>
2018-04-20 15:00:02 -0300

    [UPDATE] Alterações no nome do container e no docker-entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,8 @@
-git clone $GIT_REPOSITORY
+set -e
+
+git clone $GIT_REPOSITORY /tmp/portal-vale-cultura
+
+cp /tmp/portal-vale-cultura -r /var/wwwB
 
 cp /var/www/portal-vale-cultura/projeto/application/configs/application.ini-example /var/www/portal-vale-cultura/projeto/application/configs/application.ini
 chown www-data:www-data -R /var/www/portal-vale-cultura
