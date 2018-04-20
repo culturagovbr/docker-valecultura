5081c9297eb610090c5e962f41fceb79e45e2efe
Fernão Lopes <fernao@riseup.net>
2018-04-20 15:33:31 -0300

    [UPDATE] Verifica se já foi baixado o repositório no docker-entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,9 +1,11 @@
 set -e
 
-git clone $GIT_REPOSITORY /tmp/portal-vale-cultura
+if ! [ -d "/var/www/portal-vale-cultura" ] || ! [ -d "/var/www/portal-vale-cultura/projeto" ]; then
+    git clone $GIT_REPOSITORY /tmp/portal-vale-cultura
 
-cp /tmp/portal-vale-cultura -r /var/wwwB
+    cp /tmp/portal-vale-cultura -r /var/wwwB
 
-cp /var/www/portal-vale-cultura/projeto/application/configs/application.ini-example /var/www/portal-vale-cultura/projeto/application/configs/application.ini
-chown www-data:www-data -R /var/www/portal-vale-cultura
+    cp /var/www/portal-vale-cultura/projeto/application/configs/application.ini-example /var/www/portal-vale-cultura/projeto/application/configs/application.ini
+    chown www-data:www-data -R /var/www/portal-vale-cultura
+fi
 
